1345b78e4765b422c7ef6d41538c21e5654992de
Aleksander Brynjulf Hübert <aleksanderbrynjulfhubert@users.noreply.github.com>
2019-12-23 15:09:56 -0500

    site updated!

---
+++ js/util.js
@@ -0,0 +1,22 @@
+function toggleNav() {
+    if (document.getElementById("mySidenav").value == "0") {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "#e5e5e5";
+        document.getElementById("mySidenav").value = "1"; 
+    } else {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        document.getElementById("mySidenav").value = "0"; 
+    }
+}
+
+function hide() {
+  var x = document.getElementById("pan");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
